8dcfa4bc001c69c182e68d2ced23ee1be709e803
Luke Lopez <xlucas14@gmail.com>
2019-03-18 12:26:38 -0400

    Added Systrem tray gui

---
+++ sys_tray.py
@@ -0,0 +1,38 @@
+from pystray import *
+import pyball_protector as pyb
+from PIL import Image
+
+
+class SysTrayUI:
+    def __init__(self, screenshots_folder=None):
+        # *** Attributes ***
+        self.icon_path = "icon.png"
+        self.screenshots_folder = screenshots_folder
+        self.folder_path = "folder_path.p"
+
+        # *** Setup Menu ***
+        self.menu_items = [
+            MenuItem(self.check_status, lambda: pyb.toggle()),
+            MenuItem("Settings", lambda: print("Howdy!")),
+            MenuItem("Exit", self.exit),
+        ]
+        self.menu = Menu(*self.menu_items)
+
+        # *** Create System Tray Icon ***
+        self.icon = Icon("Test Name", menu=self.menu)
+        self.icon.icon = Image.open(self.icon_path)
+        self.icon.run()
+
+    def check_status(self, *args):
+        if pyb.Status.is_running:
+            return "Disable"
+        else:
+            return "Enable"
+
+    def exit(self, *args):
+        if pyb.Status.is_running:
+            pyb.disable()
+        self.icon.stop()
+
+
+app = SysTrayUI()
