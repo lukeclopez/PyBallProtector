26b8179d9f9c212af5b2949a1a7734aafc56c709
Luke Lopez <xlucas14@gmail.com>
2019-03-18 11:51:34 -0400

    Prepared script to integrate into UI

--- pyball_protector.py
+++ pyball_protector.py
@@ -3,19 +3,28 @@ from win10toast import ToastNotifier
 from datetime import datetime
 import os
 
+interval = (20 * 60)
+message_title = "Eye Break!"
+message_body = "Look at something 20 feet away for 20 seconds."
+
 
 def notify_for_break():
-    toaster.show_toast("Eye Break!", "Look at something 20 feet away for 20 seconds.", duration=5)
+    toaster.show_toast(message_title, message_body, duration=5)
     print('Toasted at %s' % datetime.now())
 
 
 toaster = ToastNotifier()
 scheduler = BlockingScheduler()
-scheduler.add_job(notify_for_break, 'interval', seconds=(20 * 60))
+scheduler.add_job(notify_for_break, 'interval', seconds=interval)
 
-print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
-try:
-    scheduler.start()
-except (KeyboardInterrupt, SystemExit):
+def enable():
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+
+
+def disable():
     scheduler.shutdown()
